b5afff480ef97ef8b15806ca0701106c53dd5cf2
Aday Bujeda <aday@theagilemonkeys.com>
2021-03-08 12:43:06 +0000

    Sid Landing Site - project setup

---
+++ sid-landing-site/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import './styles/index.scss';
+import App from './components/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
