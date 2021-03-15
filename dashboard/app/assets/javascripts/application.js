b779ebd7d99519ea18d46dc35bc225ead37e8157
Evan Sarmiento <evansarm@gmail.com>
2021-03-15 19:09:45 -0400

    Re-add dashboad properly

---
+++ dashboard/app/assets/javascripts/application.js
@@ -0,0 +1,136 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery3
+//= require jquery-migrate-3.1.0.min.js
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require data-confirm-modal
+//= require handlebars.js/4.4.2/handlebars.min
+//= require lodash/4.17.15/lodash.min.js
+//= require_tree .
+
+//FIXME: move to coffescript
+$(function(){
+  $('li.vdi').popover({
+    trigger: "hover",
+    content: "A VDI (Virtual Desktop Interface) gives you desktop access to a shared node. This is the graphical version of a login node. Use this for lightweight tasks like accessing & viewing files, submitting jobs, and for visualizations.",
+    title: function(){ return $(this).text() }
+  });
+
+  $('li.ihpc').popover({
+    trigger: "hover",
+    content: "An Interactive HPC session gives you dedicated access to one or more nodes on the cluster. This is similar to an interactive batch session with an accessible desktop on the primary node. Use this for heavyweight jobs such as long-running compute tasks or where you need dedicated resources.",
+    title: function(){ return $(this).text() }
+  });
+});
+
+$(document).ready(function(){
+  $('[data-toggle="popover"]').popover();
+});
+
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+/**
+ * Support persisting noVNC quality settings in localStorage
+ **/
+
+function settingKey(id) {
+  return id + '_value';
+}
+
+function storeSetting(event) {
+  var key = settingKey(event.currentTarget.id);
+  var value = event.currentTarget.value;
+
+  localStorage.setItem(key, value);
+}
+
+function tryUpdateSetting(name) {
+  var saved_value = localStorage.getItem(settingKey(name));
+
+  if(saved_value) {
+    var selector = 'input[type="range"][name="' + name + '"]';
+    $(selector).val(saved_value);
+  }
+}
+
+function installSettingHandlers(name) {
+  var selector = 'input[type="range"][name="' + name + '"]';
+  $(selector).change(storeSetting);
+}
+
+
+function promiseLoginToXDMoD(xdmodUrl){
+  return new Promise(function(resolve, reject){
+
+    var promise_to_receive_message_from_iframe = new Promise(function(resolve, reject){
+      window.addEventListener("message", function(event){
+        if (event.origin !== xdmodUrl){
+          console.log('Received message from untrusted origin, discarding');
+          return;
+        }
+        else if(event.data.application == 'xdmod'){
+          if(event.data.action == 'loginComplete'){
+            resolve();
+          }
+            else if(event.data.action == 'error'){
+              console.log('ERROR: ' + event.data.info);
+              let iframe = document.querySelector("#xdmod_login_iframe");
+              reject(new Error(`XDMoD Login iFrame at URL ${iframe && iframe.src} posted error message with info ${event.data.info}`));
+          }
+        }
+      }, false);
+    });
+
+    fetch(xdmodUrl + '/rest/auth/idpredirect?returnTo=%2Fgui%2Fgeneral%2Flogin.php')
+      .then(response => response.ok ? Promise.resolve(response) : Promise.reject())
+      .then(response => response.json())
+      .then(function(data){
+        return new Promise(function(resolve, reject){
+          var xdmodLogin = document.createElement('iframe');
+          xdmodLogin.style = 'visibility: hidden; position: absolute;left: -1000px';
+          xdmodLogin.id = 'xdmod_login_iframe'
+          xdmodLogin.src = data;
+          document.body.appendChild(xdmodLogin);
+          xdmodLogin.onload = function(){
+            resolve();
+          }
+          xdmodLogin.onerror = function(){
+            reject(new Error('Login failed: Failed to load XDMoD login page'));
+          }
+        });
+      })
+      .then(() => {
+        return Promise.race([promise_to_receive_message_from_iframe, new Promise(function(resolve, reject){
+          setTimeout(reject, 5000, new Error('Login failed: Timeout waiting for login to complete'));
+        })]);
+      })
+      .then(() => {
+        resolve();
+      })
+      .catch((e)=> {
+        reject(e);
+      });
+  });
+}
+
+var promiseLoggedIntoXDMoD = (function(){
+  return _.memoize(function(xdmodUrl){
+    return fetch(xdmodUrl + '/rest/v1/users/current', { credentials: 'include' })
+      .then(response => response.ok ? Promise.resolve() : promiseLoginToXDMoD(xdmodUrl))
+  });
+})();
---
+++ dashboard/app/controllers/batch_connect/sessions_controller.rb
@@ -0,0 +1,64 @@
+class BatchConnect::SessionsController < ApplicationController
+  include BatchConnectConcern
+
+  # GET /batch_connect/sessions
+  # GET /batch_connect/sessions.json
+  def index
+    @sessions = BatchConnect::Session.all
+    @sessions.each(&:update_cache_completed!)
+
+    set_app_groups
+    set_my_quotas
+  end
+
+  # GET /batch_connect/sessions/new
+  def new
+    @sessions = BatchConnect::Session.all
+  end
+
+  # GET /batch_connect/sessions/1
+  # GET /batch_connect/sessions/1.json
+  def show
+    set_session
+    respond_to do |format|
+      format.json { render json: {
+	"status": @session.status,
+        "id": @session.id,
+        "created_at": @session.created_at,
+        "title": @session.title,
+        "connect": @session.connect 
+      }}
+    end
+  end
+
+  # DELETE /batch_connect/sessions/1
+  # DELETE /batch_connect/sessions/1.json
+  def destroy
+    set_session
+
+    if @session.destroy
+      respond_to do |format|
+        format.html { redirect_to batch_connect_sessions_url, notice: t('dashboard.batch_connect_sessions_status_blurb_delete_success') }
+        format.json { head :no_content }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to batch_connect_sessions_url, alert: t('dashboard.batch_connect_sessions_status_blurb_delete_failure') }
+        format.json { render json: @session.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_session
+      @session = BatchConnect::Session.find(params[:id])
+    end
+
+    # Set list of app lists for navigation
+    def set_app_groups
+      @sys_app_groups = bc_sys_app_groups
+      @usr_app_groups = bc_usr_app_groups
+      @dev_app_groups = bc_dev_app_groups
+    end
+end
