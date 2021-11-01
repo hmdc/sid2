f8ea4ab11a77aabcd1285cf5e1620a744d8b4142
Aday Bujeda <aday@theagilemonkeys.com>
2021-11-01 11:23:26 +0000

    feat(dashboard): gh #450 Added FASRC cluster status to Sid homepage (#32)

---
+++ dashboard/application/app/assets/javascripts/hpc_status.js
@@ -0,0 +1,43 @@
+const HPCStatusService = (function (){
+
+    const CLUSTER_STATUS_MAP = {
+        "up": { css: "ok", text: "No known issues" },
+        "hasissues":  { css: "issues", text: "Experiencing issues" },
+        "undermaintenance":  { css: "maintenance", text: "Under maintenance" },
+        "unknown":  { css: "unknown", text: "Unknown" },
+    }
+
+    function addStatusWidget($statusContainer, status) {
+        const statusElement =
+            `<p class="hpc-status ${status.css}">
+                Current status: <span>${status.text}</span>
+             </p>`
+
+        $statusContainer.html(statusElement)
+    }
+
+    function getStatus($statusContainer) {
+        $.ajax({
+            type: 'GET',
+            url: 'https://fasrc.instatus.com/summary.json',
+        })
+            .done(data => {
+                const status = CLUSTER_STATUS_MAP[data?.page?.status?.toLowerCase()] || CLUSTER_STATUS_MAP['unknown']
+                addStatusWidget($statusContainer, status)
+            })
+            .fail(( jqxhr, settings, exception ) => {
+                console.log(`Error getting FASRC cluster status. Exception=${exception}`)
+                addStatusWidget($statusContainer, CLUSTER_STATUS_MAP['unknown'])
+            })
+    }
+
+    $(function() {
+        $("[data-toggle='hpc-status']").each((index, element) => {
+            const $statusContainer = $(element)
+            getStatus($statusContainer)
+            console.log(`HPC Status attached to: ${$statusContainer.attr("id")}`)
+        })
+    });
+
+    return { getStatus }
+})()
