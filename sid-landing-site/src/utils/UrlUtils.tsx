dbd77451acfa3bc7fd0eabae464e872e2dc0d7be
Aday Bujeda <aday@theagilemonkeys.com>
2022-03-03 12:36:02 +0000

    fix(landing-page): gh #573 Fixes to build command and login URL (#52) fix(landing-site): gh #573 fix build command fix(landing-page): gh #573 update Sid login url to point to FASSE Sid Co-authored-by: William Horka <whorka@hmdc.harvard.edu>

--- sid-landing-site/src/utils/UrlUtils.tsx
+++ sid-landing-site/src/utils/UrlUtils.tsx
@@ -4,5 +4,5 @@ export class UrlConstants {
   
   public static URL_SID_DOCUMENTATION = 'https://docs.rc.fas.harvard.edu/kb/sid-documentation/'
   public static URL_SID_SUPPORT = 'https://docs.rc.fas.harvard.edu/kb/getting-help-with-sid/'
-  public static URL_SID_CONNECT = 'https://vdi.rc.fas.harvard.edu/pun/sys/sid'
+  public static URL_SID_CONNECT = 'https://fasseood.rc.fas.harvard.edu/pun/sys/sid'
 }
